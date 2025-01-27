29e20dadbb988432a7ca285cd7876059e8cdc31f
Alannverse <alanpraseptialan@gmail.com>
2025-01-27 10:06:24 +0800

    Initial commit for new portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html", // Semua file HTML di root
+    "./src/**/*.{css,js}", // Semua file CSS dan JS di src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
